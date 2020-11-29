29d40a7c07a607805b20e182df1fdf720b6b480a
ira77kim <53447010+ira77kim@users.noreply.github.com>
2020-11-30 02:21:42 +0900

    init-first draft

---
+++ js/intro.js
@@ -0,0 +1,41 @@
+let counter =0;
+
+let intro=document.getElementById("intro");
+intro.innerHTML="As 'X' decides to dig out the time capsule,";
+
+document.getElementById("next").addEventListener("click", function(){
+    counter++;
+    console.log(counter);
+    if (counter==1){
+        intro.innerHTML="X also remembers that the time capsule was to be opened few monthes ago.";
+        intro.style.top="30vh";
+        intro.style.left="32vw";
+    }
+    else if (counter==2){
+        intro.innerHTML="X quickly finds the place where it was buried and starts digging.";
+        intro.style.top="40vh";
+        intro.style.left="40vw";
+    }
+    else if (counter==3){
+        intro.innerHTML="To others, the time capsule wasn't the most important thing at the moment, but she had to get the capsule out.";
+        intro.style.top="50vh";
+        intro.style.left="60vw";
+    }
+    else if (counter==4){
+        intro.innerHTML="shovel after shovel, as X dug deeper into the gro-- <b>THUMP.</b> <br> There's our time capsule.";
+        intro.style.top="60vh";
+        intro.style.left="68vw";
+    }
+    else if (counter==5){
+        intro.innerHTML="After digging up the time capsule, X enters the house to put the time capsule on the kitchen table.";
+        intro.style.top="70vh";
+        intro.style.left="80vw";
+    }
+    else if (counter>=6){
+        intro.innerHTML=" ";
+        document.getElementById("next").innerHTML="Open the time capsule";
+        document.getElementById("next").addEventListener("click", function(){
+            window.location="main.html";
+        })
+    }
+})
---
+++ js/sketch.js
@@ -0,0 +1,96 @@
+let counter=0;
+
+console.log(counter);
+let book = document.getElementById("book");
+let chess = document.getElementById("chess");
+let recipe = document.getElementById("recipe");
+let paper = document.getElementById("paper");
+let ring = document.getElementById("ring");
+let photo = document.getElementById("photo");
+
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// book.addEventListener("click", function(){
+//     modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// })
+book.onclick = function(){
+    counter++;
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    console.log(counter);
+    if(counter>=2){
+        document.getElementById("msg").style.opacity=1;
+    }
+}
+chess.onclick = function(){
+    counter++;
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    console.log(counter);
+    if(counter>=2){
+        document.getElementById("msg").style.opacity=1;
+    }
+}
+recipe.onclick = function(){
+    counter++;
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    console.log(counter);
+    if(counter>=2){
+        document.getElementById("msg").style.opacity=1;
+    }
+}
+paper.onclick = function(){
+    counter++;
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    console.log(counter);
+    if(counter>=2){
+        document.getElementById("msg").style.opacity=1;
+    }
+}
+ring.onclick = function(){
+    counter++;
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    console.log(counter);
+    if(counter>=2){
+        document.getElementById("msg").style.opacity=1;
+    }
+}
+photo.onclick = function(){
+    counter++;
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    console.log(counter);
+    if(counter>=2){
+        document.getElementById("msg").style.opacity=1;
+    }
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+
+
+document.getElementById("back").addEventListener("click", function(){
+    window.location="index.html";
+})
