32b8391e70f64a2e85281158dceacf2aac6bdd00
ira77kim <53447010+ira77kim@users.noreply.github.com>
2020-12-09 10:51:31 +0900

    xx

---
+++ js/last.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("back").addEventListener("click", function(){
+    window.location="index.html";
+});
+
+document.getElementById("a").addEventListener("click", function(){
+    window.location="https://www.figma.com/proto/JfpiU1zyhDitRletjFAexj/Storytelling?node-id=1%3A3&viewport=-197%2C272%2C0.23283718526363373&scaling=scale-down;"
+});
+
+document.getElementById("b").addEventListener("click", function(){
+    window.location="https://www.figma.com/proto/8m0Tl3WW4ZbTkGjqQlRsjl/Untitled?node-id=0%3A1&scaling=min-zoom;"
+});
