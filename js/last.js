660ae9f7d4ee650da3735acd0c1e8b93c8b6f8dc
ira77kim <53447010+ira77kim@users.noreply.github.com>
2020-12-16 10:17:33 +0900

    Update last.js

--- js/last.js
+++ js/last.js
@@ -1,8 +1,8 @@
 
 
-document.getElementById("back").addEventListener("click", function(){
-    window.location="index.html";
-});
+// document.getElementById("back").addEventListener("click", function(){
+//     window.location="index.html";
+// });
 
 document.getElementById("a").addEventListener("click", function(){
     window.location="https://www.figma.com/proto/JfpiU1zyhDitRletjFAexj/Storytelling?node-id=1%3A3&viewport=-197%2C272%2C0.23283718526363373&scaling=scale-down;"
